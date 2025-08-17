283641d457823d1490de29bfe468053e7ab84641
JZylber <zylberjulian@gmail.com>
2025-08-17 12:24:06 -0300

    feat: 2025 update

---
+++ dataEntry.js
@@ -0,0 +1,123 @@
+import readlineSync from "readline-sync";
+import chalk from "chalk";
+
+/**
+ * Usa la terminal para leer el nombre del cliente
+ */
+export function ingresarCliente() {
+  let cliente = readlineSync.question(chalk.yellow("¿Cuál es tu nombre? "));
+  return cliente;
+}
+
+/**
+ * Muestra por pantalla los productos disponibles
+ * @param {Array<{nombre: String, precio: Number, maxGustos: Number}>} productos - Lista de productos disponibles.
+ */
+export function mostrarProductos(productos) {
+  console.log(
+    chalk.blue(
+      "\nBienvenido heladería 'Los Pelados'! Estos son nuestros productos:\n"
+    )
+  );
+  for (let i = 0; i < productos.length; i++) {
+    console.log(
+      `${i + 1}) ${productos[i].nombre} $${productos[i].precio} (hasta ${
+        productos[i].maxGustos
+      } sabores)`
+    );
+  }
+}
+
+/**
+ * Lee la terminal para permitirle al cliente elegir un producto
+ * @param {Array<{nombre: String, precio: Number, maxGustos: Number}>} productos - Lista de productos disponibles.
+ * @returns {{nombre: String, precio: Number, maxGustos: Number}} Producto elegido.
+ */
+export function elegirProducto(productos) {
+  let productoIndex;
+  while (
+    productoIndex === undefined ||
+    productoIndex < 0 ||
+    productoIndex >= productos.length
+  ) {
+    productoIndex =
+      parseInt(
+        readlineSync.question(
+          `\n${chalk.yellow("¿Qué vas a pedir?")} ${chalk.gray(
+            "(Ingresa el número de opción)"
+          )} `
+        )
+      ) - 1;
+    if (productoIndex < 0 || productoIndex >= productos.length) {
+      console.log(
+        chalk.red("Lo siento, no entendí tu respuesta. Probá de nuevo")
+      );
+    }
+  }
+
+  let producto = productos[productoIndex];
+  return producto;
+}
+
+/**
+ * Lee la terminal para permitirle al cliente elegir una cantidad de gustos, acotada por el máximo de gustos disponibles.
+ * @param {Number} maxGustos - Máximo de gustos a elegir.
+ * @returns {Number} Número de gustos elegidos.
+ */
+export function cantidadGustos(maxGustos) {
+  let gustos;
+  while (gustos === undefined || gustos < 1 || gustos > maxGustos) {
+    gustos = parseInt(
+      readlineSync.question(chalk.yellow("¿Cuántos gustos vas a pedir? "))
+    );
+    if (gustos < 1 || gustos > maxGustos) {
+      console.log(
+        `Lo siento, solo puedes pedir entre 1 y ${maxGustos} gustos. Probá de nuevo`
+      );
+    }
+  }
+  return gustos;
+}
+
+/**
+ * Permite al usuario seleccionar una cantidad específica de sabores de una lista.
+ * Muestra los sabores disponibles y solicita al usuario que ingrese el número correspondiente a cada sabor deseado.
+ * Valida la entrada del usuario para asegurar que sea un número válido dentro del rango de opciones.
+ *
+ * @param {string[]} sabores - Array de nombres de sabores disponibles.
+ * @param {number} gustos - Cantidad de sabores que el usuario puede elegir.
+ * @returns {string[]} Array con los sabores seleccionados por el usuario.
+ */
+export function seleccionarSabores(sabores, gustos) {
+  let saboresElegidos = [];
+  console.log(chalk.blue("\nEstos son nuestros sabores:\n"));
+  for (let i = 0; i < sabores.length; i++) {
+    console.log(`${i + 1}) ${sabores[i]}`);
+  }
+  for (let i = 0; i < gustos; i++) {
+    let saborIndex;
+    while (
+      saborIndex === undefined ||
+      saborIndex < 0 ||
+      saborIndex >= sabores.length
+    ) {
+      saborIndex =
+        parseInt(
+          readlineSync.question(chalk.gray("\nIngresa el número de opción: "))
+        ) - 1;
+      if (saborIndex < 0 || saborIndex >= sabores.length) {
+        console.log(
+          chalk.red("Lo siento, no entendí tu respuesta. Probá de nuevo")
+        );
+      }
+    }
+    console.log(chalk.green(`Elegiste ${sabores[saborIndex]}`));
+    saboresElegidos.push(sabores[saborIndex]);
+  }
+  console.log(
+    chalk.blue(
+      "\n¡Gracias por comprar en heladería los pelados! Su pedido ha sido registrado"
+    )
+  );
+  return saboresElegidos;
+}
--- index.js
+++ index.js
@@ -1,68 +1,25 @@
-import readlineSync from "readline-sync";
+import {
+  cantidadGustos,
+  elegirProducto,
+  ingresarCliente,
+  mostrarProductos,
+  seleccionarSabores,
+} from "./dataEntry.js";
 
 // Cargar archivos
+// COMPLETEN USTEDES
 let sabores;
 let productos;
 
 // Ingresar cliente
-let cliente = readlineSync.question("¿Cuál es tu nombre?");
-console.log(
-  "Bienvenido heladería 'Los Pelados'! Estos son nuestros productos:"
-);
-for (let i = 0; i < productos.length; i++) {
-  console.log(
-    `${i + 1}) ${productos[i].nombre} $${productos[i].precio} (hasta ${
-      productos[i].maxGustos
-    } sabores)`
-  );
-}
+let cliente = ingresarCliente();
+// Mostrar productos
+mostrarProductos(productos);
 // Elegir producto
-let productoIndex;
-while (
-  productoIndex === undefined ||
-  productoIndex < 0 ||
-  productoIndex >= productos.length
-) {
-  productoIndex =
-    parseInt(
-      readlineSync.question("¿Qué vas a pedir? Ingresa el número de opción:")
-    ) - 1;
-  if (productoIndex < 0 || productoIndex >= productos.length) {
-    console.log("Lo siento, no entendí tu respuesta. Probá de nuevo");
-  }
-}
-
-let producto = productos[productoIndex];
+let producto = elegirProducto(productos);
 // Elegir cantidad de gustos
-let gustos;
-while (gustos === undefined || gustos < 1 || gustos > producto.maxGustos) {
-  gustos = parseInt(readlineSync.question("¿Cuántos gustos vas a pedir?"));
-  if (gustos < 1 || gustos > producto.maxGustos) {
-    console.log(
-      `Lo siento, solo puedes pedir entre 1 y ${producto.maxGustos} gustos. Probá de nuevo`
-    );
-  }
-}
+let gustos = cantidadGustos(producto.maxGustos);
 // Elegir sabores
-let saboresElegidos = [];
-console.log("Estos son nuestros sabores:");
-for (let i = 0; i < sabores.length; i++) {
-  console.log(`${i + 1}) ${sabores[i]}`);
-}
-for (let i = 0; i < gustos; i++) {
-  let saborIndex;
-  while (
-    saborIndex === undefined ||
-    saborIndex < 0 ||
-    saborIndex >= sabores.length
-  ) {
-    saborIndex =
-      parseInt(readlineSync.question("Ingresa el número de opción:")) - 1;
-    if (saborIndex < 0 || saborIndex >= sabores.length) {
-      console.log("Lo siento, no entendí tu respuesta. Probá de nuevo");
-    }
-  }
-  console.log(`Elegiste ${sabores[saborIndex]}`);
-  saboresElegidos.push(sabores[saborIndex]);
-}
+let saboresElegidos = seleccionarSabores(sabores, gustos);
 // Guardar pedido
+// COMPLETEN USTEDES
