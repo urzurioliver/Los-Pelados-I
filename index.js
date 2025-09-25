b8590b473ba7829cf5a23ecfdce65ebd2088813e
urzurioliver <urzurioliver@gmail.com>
2025-09-25 09:58:05 -0300

    base hecha

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ import {
   seleccionarProducto,
   seleccionarSabores,
 } from "./dataEntry.js";
+import fs from "fs";
 
 // Cargar sabores y productos
 // COMPLETEN USTEDES
-let sabores;
-let productos;
+let sabores = JSON.parse (fs.readFileSync("data/sabores.json", "utf-8"));
+let productos = JSON.parse (fs.readFileSync("data/productos.json", "utf-8"));;
 
 // Ingresar cliente
 let cliente = ingresarCliente();
@@ -21,3 +22,11 @@ let saboresElegidos = seleccionarSabores(sabores, gustos);
 
 // Guardar pedido
 // COMPLETEN USTEDES
+let pedidos = JSON.parse (fs.readFileSync("data/pedidos.json", "utf-8"));
+pedidos.push (cliente)
+pedidos.push (producto)
+pedidos.push (gustos)
+pedidos.push (saboresElegidos)
+
+let nuevoPedido = JSON.stringify (pedidos);
+fs.writeFileSync("data/pedidos.json", nuevoPedido)
